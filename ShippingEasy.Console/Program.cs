18533090cb05c2e1b75175b154bfce3d7dfd341b
Joshua Flanagan <joshua@shippingeasy.com>
2015-01-15 16:47:58 -0600

    Basic project structure

---
+++ ShippingEasy.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShippingEasy.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new ShippingEasy.Client();
+            System.Console.WriteLine(client.Greet());
+        }
+    }
+}
