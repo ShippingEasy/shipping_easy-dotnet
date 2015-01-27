70277de630fe51946becf93788890bc8b6d6bbf0
Joshua Flanagan <joshua@shippingeasy.com>
2015-01-26 20:36:01 -0600

    Add lots of properties to Order model

--- ShippingEasy/Order.cs
+++ ShippingEasy/Order.cs
@@ -8,10 +8,15 @@ namespace ShippingEasy
     {
         private readonly List<Recipient> _recipients = new List<Recipient>();
         [JsonProperty("id", NullValueHandling = NullValueHandling.Ignore)]
-        public int? SystemId { get; set; }
+        public int? SystemId { get; private set; }
         [JsonProperty("external_order_identifier")]
         public string OrderIdentifier { get; set; }
         public DateTimeOffset? OrderedAt { get; set; }
+        /// <summary>
+        /// Time order was last updated by ShippingEasy.
+        /// </summary>
+        [JsonProperty]
+        public DateTimeOffset? UpdatedAt { get; private set; }
 
         public List<Recipient> Recipients
         {
@@ -22,6 +27,38 @@ namespace ShippingEasy
         public string Status { get; set; }
 
         public decimal? TotalIncludingTax { get; set; }
+        public decimal? TotalExcludingTax { get; set; }
+        public decimal? DiscountAmount { get; set; }
+        public decimal? CouponDiscount { get; set; }
+        public decimal? SubtotalIncludingTax { get; set; }
+        public decimal? SubtotalExcludingTax { get; set; }
+        public decimal? SubtotalTax { get; set; }
+        public decimal? TotalTax { get; set; }
+        public decimal? BaseShippingCost { get; set; }
+        public decimal? ShippingCostIncludingTax { get; set; }
+        public decimal? ShippingCostExcludingTax { get; set; }
+        public decimal? ShippingCostTax { get; set; }
+        public decimal? BaseHandlingCost { get; set; }
+        public decimal? HandlingCostExcludingTax { get; set; }
+        public decimal? HandlingCostIncludingTax { get; set; }
+        public decimal? HandlingCostTax { get; set; }
+        public decimal? BaseWrappingCost { get; set; }
+        public decimal? WrappingCostExcludingTax { get; set; }
+        public decimal? WrappingCostIncludingTax { get; set; }
+        public decimal? WrappingCostTax { get; set; }
+        public string BillingCompany { get; set; }
+        public string BillingFirstName { get; set; }
+        public string BillingLastName { get; set; }
+        public string BillingAddress { get; set; }
+        public string BillingAddress2 { get; set; }
+        public string BillingCity { get; set; }
+        public string BillingState { get; set; }
+        public string BillingCountry { get; set; }
+        public string BillingPostalCode { get; set; }
+        public string BillingPhoneNumber { get; set; }
+        public string BillingEmail { get; set; }
+        [JsonProperty]
+        public string StoreApiKey { get; private set; }
     }
 
     public class Recipient
@@ -31,12 +68,45 @@ namespace ShippingEasy
         public string LastName { get; set; }
         public string Address { get; set; }
         public string Address2 { get; set; }
-        public List<LineItem> LineItems { get { return _lineItems; } } 
+        public List<LineItem> LineItems { get { return _lineItems; } }
+        public string Address3 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public bool? Residential { get; set; }
+        public string Province { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        [JsonProperty("postal_code_plus_4")]
+        public string PostalCodePlus4 { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public decimal? BaseCost { get; set; }
+        public decimal? CostExcludingTax { get; set; }
+        public decimal? CostIncludingTax { get; set; }
+        public decimal? CostTax { get; set; }
+        public decimal? BaseHandlingCost { get; set; }
+        public decimal? HandlingCostIncludingTax { get; set; }
+        public decimal? HandlingCostExcludingTax { get; set; }
+        public decimal? HandlingCostTax { get; set; }
+        public int? ShippingZoneId { get; set; }
+        public string ShippingZoneName { get; set; }
+        public int? ItemsTotal { get; set; }
+        public string ShippingMethod { get; set; }
+        public int? ItemsShipped { get; set; }
+        public int? ExtShippingDetailId { get; set; }
     }
 
     public class LineItem
     {
         public string ItemName { get; set; }
         public int? Quantity { get; set; }
+        public string Sku { get; set; }
+        public string BinPickingNumber { get; set; }
+        public decimal? WeightInOunces { get; set; }
+        public decimal? TotalExcludingTax { get; set; }
+        public decimal? PriceExcludingTax { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public string ExtLineItemId { get; set; }
+        public string ExtProductId { get; set; }
     }
 }
--- Tests/CreateOrderResponseTests.cs
+++ Tests/CreateOrderResponseTests.cs
@@ -1,7 +1,8 @@
-﻿using NUnit.Framework;
-using ShippingEasy;
-using System;
+﻿using System;
+using System.Configuration;
 using System.IO;
+using NUnit.Framework;
+using ShippingEasy;
 
 namespace Tests
 {
@@ -19,18 +20,94 @@ namespace Tests
             var date = new DateTimeOffset(2015, 1, 23, 20, 13, 32, new TimeSpan());
             Assert.AreEqual(date, order.OrderedAt);
             Assert.AreEqual("ready_for_shipment", order.Status);
-            Assert.AreEqual(0.01, order.TotalIncludingTax);
+            Assert.AreEqual(0.01m, order.TotalIncludingTax);
+            Assert.AreEqual(0.02m, order.TotalExcludingTax);
+            Assert.AreEqual(0.03m, order.DiscountAmount);
+            Assert.AreEqual(0.04m, order.CouponDiscount);
+            Assert.AreEqual(0.05m, order.SubtotalIncludingTax);
+            Assert.AreEqual(0.06m, order.SubtotalExcludingTax);
+            Assert.AreEqual(0.07m, order.SubtotalTax);
+            Assert.AreEqual(0.08m, order.TotalTax);
+            Assert.AreEqual(0.09m, order.BaseShippingCost);
+            Assert.AreEqual(0.10m, order.ShippingCostIncludingTax);
+            Assert.AreEqual(0.11m, order.ShippingCostExcludingTax);
+            Assert.AreEqual(0.12m, order.ShippingCostTax);
+            Assert.AreEqual(0.13m, order.BaseHandlingCost);
+            Assert.AreEqual(0.14m, order.HandlingCostExcludingTax);
+            Assert.AreEqual(0.15m, order.HandlingCostIncludingTax);
+            Assert.AreEqual(0.16m, order.HandlingCostTax);
+            Assert.AreEqual(0.17m, order.BaseWrappingCost);
+            Assert.AreEqual(0.18m, order.WrappingCostExcludingTax);
+            Assert.AreEqual(0.19m, order.WrappingCostIncludingTax);
+            Assert.AreEqual(0.20m, order.WrappingCostTax);
+            Assert.AreEqual("The Bills", order.BillingCompany);
+            Assert.AreEqual("Jim", order.BillingFirstName);
+            Assert.AreEqual("Kelly", order.BillingLastName);
+            Assert.AreEqual("100 Cold Lake Way", order.BillingAddress);
+            Assert.AreEqual("Suite #0", order.BillingAddress2);
+            Assert.AreEqual("Buffalo", order.BillingCity);
+            Assert.AreEqual("NY", order.BillingState);
+            Assert.AreEqual("USA", order.BillingCountry);
+            Assert.AreEqual("19901", order.BillingPostalCode);
+            Assert.AreEqual("2124443333", order.BillingPhoneNumber);
+            Assert.AreEqual("bills@example.com", order.BillingEmail);
+            Assert.AreEqual("c71dc6da574eea04e2c926906bcb4eab", order.StoreApiKey);
+            var updated = new DateTimeOffset(2015, 1, 23, 20, 13, 33, new TimeSpan());
+            Assert.AreEqual(updated, order.UpdatedAt);
+
 
             Assert.AreEqual(1, order.Recipients.Count);
             var recipient = order.Recipients[0];
 
             Assert.AreEqual("Colin", recipient.FirstName);
             Assert.AreEqual("Smith", recipient.LastName);
-            Assert.AreEqual(1, recipient.LineItems.Count);
-            var lineItem = recipient.LineItems[0];
+            Assert.AreEqual("1600 Pennsylvania Ave", recipient.Address);
+            Assert.AreEqual("Suite # 2", recipient.Address2);
+            Assert.AreEqual("c/o White House", recipient.Address3);
+            Assert.AreEqual("Washington", recipient.City);
+            Assert.AreEqual("DC", recipient.State);
+            Assert.AreEqual(true, recipient.Residential);
+            Assert.AreEqual("Province", recipient.Province);
+            Assert.AreEqual("United States", recipient.Country);
+            Assert.AreEqual("12345", recipient.PostalCode);
+            Assert.AreEqual("5250", recipient.PostalCodePlus4);
+            Assert.AreEqual("5553339999", recipient.PhoneNumber);
+            Assert.AreEqual("colin@example.com", recipient.Email);
+            Assert.AreEqual(0.00m, recipient.BaseCost);
+            Assert.AreEqual(1.00m, recipient.CostExcludingTax);
+            Assert.AreEqual(2.00m, recipient.CostIncludingTax);
+            Assert.AreEqual(3.00m, recipient.CostTax);
+            Assert.AreEqual(4.00m, recipient.BaseHandlingCost);
+            Assert.AreEqual(5.00m, recipient.HandlingCostExcludingTax);
+            Assert.AreEqual(6.00m, recipient.HandlingCostIncludingTax);
+            Assert.AreEqual(7.00m, recipient.HandlingCostTax);
+            Assert.AreEqual(4, recipient.ShippingZoneId);
+            Assert.AreEqual("North", recipient.ShippingZoneName);
+            Assert.AreEqual(88, recipient.ItemsTotal);
+            Assert.AreEqual("Fast", recipient.ShippingMethod);
+            Assert.AreEqual(66, recipient.ItemsShipped);
+            Assert.AreEqual(33, recipient.ExtShippingDetailId);
 
-            Assert.AreEqual("Sprocket", lineItem.ItemName);
-            Assert.AreEqual(7, lineItem.Quantity);
+
+            Assert.AreEqual(2, recipient.LineItems.Count);
+
+            var line1 = recipient.LineItems[0];
+            Assert.AreEqual("Sprocket", line1.ItemName);
+            Assert.AreEqual(null, line1.Sku);
+            Assert.AreEqual("W4", line1.BinPickingNumber);
+            Assert.AreEqual(16.5m, line1.WeightInOunces);
+            Assert.AreEqual(7, line1.Quantity);
+            Assert.AreEqual(1.01m, line1.TotalExcludingTax);
+            Assert.AreEqual(1.02m, line1.PriceExcludingTax);
+            Assert.AreEqual(1.03m, line1.UnitPrice);
+            Assert.AreEqual("998", line1.ExtLineItemId);
+            Assert.AreEqual("887", line1.ExtProductId);
+            var line2 = recipient.LineItems[1];
+            Assert.AreEqual("Widget", line2.ItemName);
+            Assert.AreEqual("AB-321", line2.Sku);
+            Assert.AreEqual(null, line2.BinPickingNumber);
+            Assert.AreEqual(4.01m, line2.TotalExcludingTax);
+
         }
     }
 }
